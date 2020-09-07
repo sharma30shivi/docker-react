3195d62e5afe9005716ff8c7e035e7a8cffa9292
sharma30shivi@gmail.com <harinder.dhani23@gmail.com>
2020-09-07 00:24:06 -0400

    changed app text

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Mohit Karan Shivi Harry Ankit
         </a>
       </header>
     </div>
